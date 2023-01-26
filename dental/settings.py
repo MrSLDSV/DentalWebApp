0a45fd713261373b292f61bac4564c11c788a3fb
MrSLDSV <mrsldsv@protonmail.com>
2023-01-26 00:02:59 +0000

    Reverted changes done to email configuration in settings.py

--- dental/settings.py
+++ dental/settings.py
@@ -139,7 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'socratesviegas2@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_PASSWORD = 'wkzqczuxkjwjhhtg'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
